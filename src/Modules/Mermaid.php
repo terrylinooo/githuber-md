df2540ccf27da74ca2af6eadd2f84c5c09153030
terrylinooo <terrylin.developer@gmail.com>
2018-12-30 00:04:51 +0800

    Add new module: Mermaid.js

---
+++ src/Modules/Mermaid.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Module Name: Mermaid
+ * Module Description: Turn text into vector UML sequence diagrams.
+ *
+ * @author Terry Lin
+ * @link https://terryl.in/
+ *
+ * @package Githuber
+ * @since 1.4.0
+ * @version 1.4.0
+ */
+
+namespace Githuber\Module;
+
+class Mermaid extends ModuleAbstract {
+
+	/**
+	 * The version of flowchart.js we are using.
+	 *
+	 * @var string
+	 */
+    public $mermaid_version = '8.0.0';
+
+	/**
+	 * Constructer.
+	 */
+	public function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * Constants.
+	 */
+	const MD_POST_META_MERMAID = '_is_githuber_mermaid';
+
+	/**
+	 * Initialize.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'front_enqueue_scripts' ) );
+		add_action( 'wp_print_footer_scripts', array( $this, 'front_print_footer_scripts' ) );
+	}
+ 
+	/**
+	 * Register CSS style files for frontend use.
+	 * 
+	 * @return void
+	 */
+	public function front_enqueue_styles() {
+
+	}
+
+	/**
+	 * Register JS files for frontend use.
+	 * 
+	 * @return void
+	 */
+	public function front_enqueue_scripts() {
+		if ( $this->is_module_should_be_loaded( self::MD_POST_META_MERMAID ) ) {
+			$option = githuber_get_option( 'mermaid_src', 'githuber_modules' );
+
+			switch ( $option ) {
+                case 'cloudflare':
+                    $script_url = 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/' . $this->mermaid_version . '/mermaid.min.js';
+                    break;
+
+				case 'jsdelivr':
+                    $script_url = 'https://cdn.jsdelivr.net/npm/mermaid@' . $this->mermaid_version . '/dist/mermaid.min.js';
+					break;
+
+				default:
+					$script_url = $this->githuber_plugin_url . 'assets/vendor/mermaid/mermaid.min.js';
+					break;
+			} 
+
+			wp_enqueue_script( 'mermaid', $script_url, array(), $this->mermaid_version, true );
+		}
+	}
+
+	/**
+	 * Print Javascript plaintext in page footer.
+	 */
+	public function front_print_footer_scripts() {
+		$script = '
+			<script id="module-mermaid">
+				(function($) {
+					$(function() {
+						if (typeof mermaid !== "undefined") {
+							mermaid.initialize();
+						}
+					});
+                })(jQuery);
+			</script>
+		';
+		echo $script;
+	}
+}
