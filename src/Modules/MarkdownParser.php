ccbd472753f24d4ebd43eaedd46a45e160fc4a8c
terrylinooo <terrylin.developer@gmail.com>
2019-01-15 23:01:30 +0800

    Fall back to use  Parsedown if PHP version < 5.3.6, ParsedownExtra uses DOMDocument::saveHTML

--- src/Modules/MarkdownParser.php
+++ src/Modules/MarkdownParser.php
@@ -8,38 +8,65 @@
  *
  * @package Githuber
  * @since 1.0.0
- * @version 1.0.0
+ * @version 1.4.3
  */
 
 namespace Githuber\Module;
-use ParsedownExtra;
 
-class MarkdownParser extends ParsedownExtra {
+if ( version_compare( phpversion(), '5.3.6', '>=' ) ) {
 
-	/**
-	 * Constructer.
-	 */
-	public function __construct() {
-		parent::__construct();
+	class MarkdownParser extends \ParsedownExtra {
+		/**
+		 * Constructer.
+		 */
+		public function __construct() {
+			parent::__construct();
+		}
+	
+		/**
+		 * Remove bare <p> elements. <p>s with attributes will be preserved.
+		 *
+		 * @param  string $text HTML content.
+		 * @return string <p>-less content.
+		 */
+		public function remove_bare_p_tags( $text ) {
+			return preg_replace( "#<p>(.*?)</p>(\n|$)#ums", '$1$2', $text );
+		}
+	
+		/**
+		 * Teansform Markdown to HTML.
+		 * 
+		 * @param string $text Markdown content.
+		 */
+		public function transform( $text ) {
+			$parsed_content = $this->text( $text );
+			return $parsed_content;
+		}
 	}
 
-	/**
-	 * Remove bare <p> elements. <p>s with attributes will be preserved.
-	 *
-	 * @param  string $text HTML content.
-	 * @return string <p>-less content.
-	 */
-	public function remove_bare_p_tags( $text ) {
-		return preg_replace( "#<p>(.*?)</p>(\n|$)#ums", '$1$2', $text );
-	}
+} else {
 
-	/**
-	 * Teansform Markdown to HTML.
-	 * 
-	 * @param string $text Markdown content.
-	 */
-	public function transform( $text ) {
-		$parsed_content = $this->text( $text );
-		return $parsed_content;
+	class MarkdownParser extends \Parsedown {
+
+		/**
+		 * Remove bare <p> elements. <p>s with attributes will be preserved.
+		 *
+		 * @param  string $text HTML content.
+		 * @return string <p>-less content.
+		 */
+		public function remove_bare_p_tags( $text ) {
+			return preg_replace( "#<p>(.*?)</p>(\n|$)#ums", '$1$2', $text );
+		}
+	
+		/**
+		 * Teansform Markdown to HTML.
+		 * 
+		 * @param string $text Markdown content.
+		 */
+		public function transform( $text ) {
+			$parsed_content = $this->text( $text );
+			return $parsed_content;
+		}
 	}
 }
+
