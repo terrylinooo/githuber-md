76703c8f09f2b2737e464c0468b930b11637ea9c
terrylinooo <terrylin.developer@gmail.com>
2020-04-15 00:39:30 +0800

    Update version number.

--- src/Controllers/Markdown.php
+++ src/Controllers/Markdown.php
@@ -27,7 +27,7 @@ class Markdown extends ControllerAbstract {
 	 *
 	 * @var string
 	 */
-	public $editormd_varsion = '1.5.0.12';
+	public $editormd_varsion = '1.5.0.13';
 
 	/**
 	 * The Post Type support from Markdown controller.
