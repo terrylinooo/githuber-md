18d33f50f189ae7b4057877ef0e94e1c6fe7d9c0
terrylinooo <terrylin.developer@gmail.com>
2020-04-15 22:00:18 +0800

    Remove a identification code.

--- src/Modules/Highlight.php
+++ src/Modules/Highlight.php
@@ -55,7 +55,6 @@ class Highlight extends ModuleAbstract {
 		'basic'          => 'Basic',
 		'bnf'            => 'Backus–Naur Form',
 		'brainfuck'      => 'Brainfuck',
-		'c'              => 'C',
 		'cal'            => 'C/AL',
 		'capnproto'      => 'Cap’n Proto',
 		'ceylon'         => 'Ceylon',
