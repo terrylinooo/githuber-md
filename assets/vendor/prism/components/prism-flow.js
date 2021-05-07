cf1427e6e96564e392e1d6577dac40c1010aa550
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2021-05-07 17:56:14 +0200

    updated prismjs assets to 1.23.0
    using the following command:
    
    ```bash
    curl https://registry.npmjs.org/prismjs/-/prismjs-1.23.0.tgz \
      tar --strip-components 1 -C assets/vendor/prism -xzf -
    ```

--- assets/vendor/prism/components/prism-flow.js
+++ assets/vendor/prism/components/prism-flow.js
@@ -9,7 +9,8 @@
 			}
 		]
 	});
-	Prism.languages.flow['function-variable'].pattern = /[_$a-z\xA0-\uFFFF][$\w\xA0-\uFFFF]*(?=\s*=\s*(?:function\b|(?:\([^()]*\)(?:\s*:\s*\w+)?|[_$a-z\xA0-\uFFFF][$\w\xA0-\uFFFF]*)\s*=>))/i;
+	Prism.languages.flow['function-variable'].pattern = /(?!\s)[_$a-z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*=\s*(?:function\b|(?:\([^()]*\)(?:\s*:\s*\w+)?|(?!\s)[_$a-z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*)\s*=>))/i;
+	delete Prism.languages.flow['parameter'];
 
 	Prism.languages.insertBefore('flow', 'operator', {
 		'flow-punctuation': {
@@ -18,7 +19,7 @@
 		}
 	});
 
-	if (Prism.util.type(Prism.languages.flow.keyword) !== 'Array') {
+	if (!Array.isArray(Prism.languages.flow.keyword)) {
 		Prism.languages.flow.keyword = [Prism.languages.flow.keyword];
 	}
 	Prism.languages.flow.keyword.unshift(
